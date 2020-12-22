a9f38231ff8154b40d87d47b8a26231be939fbf4
Joan Baza <iijoanjuan10@gmail.com>
2020-12-22 17:55:19 +0100

    Ejercicio 1

---
+++ EJ1/EJ1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(); // Para la ejecucción hasta presionar tecla.
+        }
+    }
+}
