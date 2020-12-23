b8cd65fd707697d8dd2a46c912fbb5405389a287
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-23 10:27:27 +0100

    Ejercicio 1

---
+++ UD03_Ej1/UD03_Ej1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UD03_Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 4;
+            int numero2 = 2;
+
+            Console.WriteLine("Suma: {0} + {1} = {2}", numero, numero2, numero + numero2);
+            Console.WriteLine("Resta: {0} - {1} = {2}", numero, numero2, numero - numero2);
+            Console.WriteLine("Multiplicacion: {0} * {1} = {2}", numero, numero2, numero * numero2);
+            Console.WriteLine("Division: {0} / {1} = {2}", numero, numero2, numero / numero2);
+            Console.WriteLine("Residuo: {0} % {1} = {2}", numero, numero2, numero % numero2);
+        }
+    }
+}
