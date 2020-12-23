38a25cda6d74ed06784d7cdf3a99fd3b7867bade
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-23 12:00:53 +0100

    Ejercicio4

---
+++ UD03_Ej4/UD03_Ej4/UD03_Ej4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UD03_Ej4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+
+            // Mostramos valor inicial
+            Console.WriteLine("Valor inicial de N = {0}", n);
+
+            // Valor sumado a 77
+            n += 77;
+            Console.WriteLine("N + 77 = {0}", n);
+
+            // Valor restado a 3
+            n -= 3;
+            Console.WriteLine("N - 3 = {0}", n);
+
+            // Valor multiplicado por 2
+            n *= 2;
+            Console.WriteLine("N * 2 = {0}", n);
+
+        }
+    }
+}
