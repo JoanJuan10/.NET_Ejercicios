555ef0129a5fefb30920b262c320f6c5edfce8a5
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-23 11:16:31 +0100

    Ejercicio 5

---
+++ UD03_Ej5/UD03_Ej5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UD03_Ej5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            int c = 6;
+            int d = 10;
+
+            int guardarB = b;
+
+            b = c;
+            c = a;
+            a = d;
+            d = guardarB;
+
+            Console.WriteLine("A = {0}", a);
+            Console.WriteLine("B = {0}", b);
+            Console.WriteLine("C = {0}", c);
+            Console.WriteLine("D = {0}", d);
+        }
+    }
+}
